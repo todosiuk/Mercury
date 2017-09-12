b319ce629d411f1819718b5a9eb9baa9c51e946c
todosuk <todosuk@VZ-130-06.epicentrk.ua>
2017-09-12 17:11:02 +0300

    Commit 7

---
+++ src/main/java/org/myapp/mercury/application/model/entity/logistics/Supplier.java
@@ -0,0 +1,13 @@
+package org.myapp.mercury.application.model.entity.logistics;
+
+import org.myapp.mercury.application.model.entity.base.AbstractEntity;
+
+/**
+ * Supplier of goods
+ * 
+ * @author todosuk
+ *
+ */
+public class Supplier extends AbstractEntity {
+
+}
---
+++ src/main/java/org/myapp/mercury/application/model/entity/logistics/Supply.java
@@ -0,0 +1,12 @@
+package org.myapp.mercury.application.model.entity.logistics;
+
+import org.myapp.mercury.application.model.entity.base.AbstractEntity;
+
+/**
+ * Goods supply
+ * @author todosuk
+ *
+ */
+public class Supply extends AbstractEntity {
+
+}
