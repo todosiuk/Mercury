2e07c263de2b34e47b10cd6fb59c2edba2b8a8a7
todosuk <todosuk@VZ-130-06.epicentrk.ua>
2017-09-13 16:25:37 +0300

    Created supplier and supply classes

--- src/main/java/org/myapp/mercury/application/model/entity/logistics/Supplier.java
+++ src/main/java/org/myapp/mercury/application/model/entity/logistics/Supplier.java
@@ -1,5 +1,9 @@
 package org.myapp.mercury.application.model.entity.logistics;
 
+import java.awt.List;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.myapp.mercury.application.model.entity.base.AbstractEntity;
 
 /**
@@ -10,4 +14,44 @@ import org.myapp.mercury.application.model.entity.base.AbstractEntity;
  */
 public class Supplier extends AbstractEntity {
 
+	private String name;
+
+	private Set<Supply> supplyList;
+
+	public Supplier(String string) {
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<Supply> getSupplyList() {
+		return supplyList;
+	}
+
+	public void setSupplyList(Set<Supply> supplyList) {
+		this.supplyList = supplyList;
+	}
+
+	/**
+	 * Adds supply to the supply list
+	 * 
+	 * @param supply
+	 * @return
+	 */
+	public Supply addSupply(final String carsNumber) {
+		if (supplyList == null) {
+			supplyList = new HashSet<Supply>();
+		}
+		Supply supply = new Supply(this, carsNumber);
+		supplyList.add(supply);
+		return supply;
+	}
+	/**
+	 * Remove supply
+	 */
 }
