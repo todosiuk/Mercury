5aaa754fb14054c368a9e3e9cdbc8b11f7dee007
Volodymyr Todosiuk <v.todosiuk@gmail.com>
2017-09-15 19:49:13 +0300

    Commit 10

---
+++ mercury-service/src/main/java/org/mercury/service/App.java
@@ -0,0 +1,13 @@
+package org.mercury.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
