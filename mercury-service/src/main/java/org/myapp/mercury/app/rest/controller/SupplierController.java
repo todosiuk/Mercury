1417f05387ef6fff515284344e6b01b5da293ea6
Volodymyr Todosiuk <v.todosiuk@gmail.com>
2017-09-23 13:49:18 +0300

    Commit 25

--- mercury-service/src/main/java/org/myapp/mercury/app/rest/controller/SupplierController.java
+++ mercury-service/src/main/java/org/myapp/mercury/app/rest/controller/SupplierController.java
@@ -2,18 +2,12 @@ package org.myapp.mercury.app.rest.controller;
 
 import java.util.List;
 
-import org.myapp.mercury.app.model.entity.logistic.Supplier;
 import org.myapp.mercury.app.service.LogisticService;
-import org.myapp.mercury.app.service.impl.LogisticServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.google.common.collect.Lists;
 
 @RestController
 @RequestMapping(value = "/api")
--- mercury-service/src/main/java/org/myapp/mercury/transform/impl/DTOTransformer.java
+++ mercury-service/src/main/java/org/myapp/mercury/transform/impl/DTOTransformer.java
@@ -1,10 +1,13 @@
 package org.myapp.mercury.transform.impl;
 
 import org.myapp.mercury.app.infrastructure.util.Checks;
+import org.myapp.mercury.app.infrastructure.util.CommonUtil;
 import org.myapp.mercury.app.infrastructure.util.ReflectionUtil;
 import org.myapp.mercury.app.model.entity.base.AbstractEntity;
 import org.myapp.mercury.app.rest.dto.base.BaseDTO;
 import org.myapp.mercury.transform.Transformer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Default transformation engine that uses reflection to transform objects
@@ -13,6 +16,7 @@ import org.myapp.mercury.transform.Transformer;
  *
  */
 public class DTOTransformer implements Transformer {
+	private static final Logger LOGGER = LoggerFactory.getLogger(DTOTransformer.class);
 
 	@Override
 	public <T extends AbstractEntity, P extends BaseDTO<T>> P transform(final T entity, final Class<P> clz) {
@@ -39,6 +43,10 @@ public class DTOTransformer implements Transformer {
 
 		ReflectionUtil.copyFields(dto, entity, ReflectionUtil.findSimilarFields(dto.getClass(), clz));
 		dto.untransform(entity);
+
+		if (LOGGER.isDebugEnabled()) {
+			LOGGER.debug("SimpleDTOTransformer.transform: {} entity", CommonUtil.toString(dto));
+		}
 
 		return entity;
 	}
