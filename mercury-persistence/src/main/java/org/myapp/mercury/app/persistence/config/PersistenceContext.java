90977f5de00a9cf24f99718cf072b6e95b63fa96
Volodymyr Todosiuk <v.todosiuk@gmail.com>
2017-09-26 12:39:29 +0300

    Commit 26

---
+++ mercury-persistence/src/main/java/org/myapp/mercury/app/persistence/config/PersistenceContext.java
@@ -0,0 +1,44 @@
+package org.myapp.mercury.app.persistence.config;
+
+import java.util.Properties;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+public class PersistenceContext {
+	@Bean
+	public DriverManagerDataSource getDataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/javasavvy");
+		dataSource.setUsername("root");
+		dataSource.setPassword("root");
+		return dataSource;
+	}
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean getEMF() {
+
+		LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+		emf.setDataSource(getDataSource());
+		emf.setPersistenceUnitName("spring-jpa-unit");
+		emf.setJpaVendorAdapter(getHibernateAdapter());
+		Properties jpaProperties = new Properties();
+		jpaProperties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+		jpaProperties.put("hibernate.hbm2ddl.auto", "create");
+		jpaProperties.put("hibernate.show_sql", "true");
+		jpaProperties.put("hibernate.format_sql", "false");
+		emf.setJpaProperties(jpaProperties);
+		return emf;
+	}
+
+	@Bean
+	public JpaVendorAdapter getHibernateAdapter() {
+		return new HibernateJpaVendorAdapter();
+	}
+}
