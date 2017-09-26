80ce29f19c81a3833da8f18dd771fb32d9c0fb57
Volodymyr Todosiuk <v.todosiuk@gmail.com>
2017-09-26 12:46:49 +0300

    Commit 27

--- mercury-persistence/src/main/java/org/myapp/mercury/app/persistence/config/PersistenceContext.java
+++ mercury-persistence/src/main/java/org/myapp/mercury/app/persistence/config/PersistenceContext.java
@@ -2,14 +2,21 @@ package org.myapp.mercury.app.persistence.config;
 
 import java.util.Properties;
 
+import javax.persistence.EntityManagerFactory;
+
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
+@ComponentScan(basePackages = { "org.myapp.mercury" })
+@EnableTransactionManagement
 public class PersistenceContext {
 	@Bean
 	public DriverManagerDataSource getDataSource() {
@@ -40,5 +47,12 @@ public class PersistenceContext {
 	@Bean
 	public JpaVendorAdapter getHibernateAdapter() {
 		return new HibernateJpaVendorAdapter();
+	}
+
+	@Bean
+	JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(entityManagerFactory);
+		return transactionManager;
 	}
 }
