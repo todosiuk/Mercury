7de0e39d617dccc8ae1614c846c48900f4be592c
Volodymyr Todosiuk <v.todosiuk@gmail.com>
2017-09-28 18:03:18 +0300

    Commit 29

--- mercury-service/src/test/java/org/myapp/mercury/app/service/LogisticServiceImplTest.java
+++ mercury-service/src/test/java/org/myapp/mercury/app/service/LogisticServiceImplTest.java
@@ -8,13 +8,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.myapp.mercury.app.model.entity.logistic.Supplier;
 import org.myapp.mercury.app.rest.config.MercuryConfig;
-import org.myapp.mercury.app.rest.config.MercuryConfiguration;
 import org.myapp.mercury.app.rest.config.MercuryInitializer;
 import org.myapp.mercury.app.service.impl.LogisticServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 /**
  * Contain unit- tests for {@link LogisticServiceImpl}
@@ -23,6 +23,7 @@ import org.springframework.test.context.support.AnnotationConfigContextLoader;
  *
  */
 @RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
 @ContextConfiguration(classes = { MercuryConfig.class,
 		MercuryInitializer.class }, loader = AnnotationConfigContextLoader.class)
 public class LogisticServiceImplTest {
