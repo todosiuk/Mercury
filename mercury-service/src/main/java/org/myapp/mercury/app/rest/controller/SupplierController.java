9c6e8f03a4c15427206406462c140907e0c4a45f
Volodymyr Todosiuk <v.todosiuk@gmail.com>
2017-10-11 14:10:31 +0300

    Commit 35

--- mercury-service/src/main/java/org/myapp/mercury/app/rest/controller/SupplierController.java
+++ mercury-service/src/main/java/org/myapp/mercury/app/rest/controller/SupplierController.java
@@ -1,23 +1,47 @@
-/**package org.myapp.mercury.app.rest.controller;
+package org.myapp.mercury.app.rest.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.myapp.mercury.app.model.entity.logistic.Supplier;
+import org.myapp.mercury.app.rest.dto.SupplierDTO;
 import org.myapp.mercury.app.service.LogisticService;
+import org.myapp.mercury.app.transform.Transformer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/api")
 public class SupplierController {
 
 	@Autowired
 	LogisticService logisticService;
 
-	@RequestMapping(value = "/suppliers", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<String> findSuppliers() {
-		return null;
+	private final Transformer transformer;
+
+	public SupplierController(LogisticService logisticService, Transformer transformer) {
+		this.transformer = transformer;
+		this.logisticService = logisticService;
 	}
-}*/
+
+	@GetMapping("/suppliers")
+	public List<SupplierDTO> findSuppliers() {
+		return logisticService.findSuppliers().stream()
+				.map((supplier) -> transformer.transform(supplier, SupplierDTO.class)).collect(Collectors.toList());
+	}
+
+	@PostMapping("/saveSupplier")
+	public void saveSupplier(SupplierDTO supplierDTO) {
+		logisticService.saveSupplier(transformer.untransform(supplierDTO, Supplier.class));
+	}
+
+	public ResponseEntity deleteSupplier(@PathVariable int id) {
+		return null;
+		
+	}
+
+}
