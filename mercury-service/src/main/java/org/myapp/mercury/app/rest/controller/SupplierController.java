537114b6b4ba765d1b0216ab20cc0133cff9e44e
Volodymyr Todosiuk <v.todosiuk@gmail.com>
2017-10-12 16:53:46 +0300

    Commit 36

--- mercury-service/src/main/java/org/myapp/mercury/app/rest/controller/SupplierController.java
+++ mercury-service/src/main/java/org/myapp/mercury/app/rest/controller/SupplierController.java
@@ -1,8 +1,8 @@
 package org.myapp.mercury.app.rest.controller;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
-
 import org.myapp.mercury.app.model.entity.logistic.Supplier;
 import org.myapp.mercury.app.rest.dto.SupplierDTO;
 import org.myapp.mercury.app.service.LogisticService;
@@ -10,6 +10,7 @@ import org.myapp.mercury.app.transform.Transformer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,9 +30,13 @@ public class SupplierController {
 	}
 
 	@GetMapping("/suppliers")
-	public List<SupplierDTO> findSuppliers() {
-		return logisticService.findSuppliers().stream()
+	public ResponseEntity<List<SupplierDTO>> findSuppliers() {
+		List<SupplierDTO> suppliers = logisticService.findSuppliers().stream()
 				.map((supplier) -> transformer.transform(supplier, SupplierDTO.class)).collect(Collectors.toList());
+		if (suppliers.isEmpty()) {
+			return new ResponseEntity<List<SupplierDTO>>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<List<SupplierDTO>>(suppliers, HttpStatus.OK);
 	}
 
 	@PostMapping("/saveSupplier")
@@ -39,9 +44,14 @@ public class SupplierController {
 		logisticService.saveSupplier(transformer.untransform(supplierDTO, Supplier.class));
 	}
 
-	public ResponseEntity deleteSupplier(@PathVariable int id) {
-		return null;
-		
+	@DeleteMapping("/deleteSupplier/{id}")
+	public ResponseEntity<Supplier> deleteSupplier(@PathVariable int id) {
+		Optional<Supplier> supplier = logisticService.findSupplierById(id);
+		if (supplier == null) {
+			return new ResponseEntity<Supplier>(HttpStatus.NOT_FOUND);
+		}
+		logisticService.deleteSupplier(id);
+		return new ResponseEntity<Supplier>(HttpStatus.NO_CONTENT);
 	}
 
 }
