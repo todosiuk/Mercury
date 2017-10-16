042917b5de28b224597d4cd60ef98c7da05f1824
Volodymyr Todosiuk <v.todosiuk@gmail.com>
2017-10-16 16:45:01 +0300

    Commit 39

--- mercury-service/src/test/java/org/myapp/mercury/app/rest/controller/SupplierControllerTest.java
+++ mercury-service/src/test/java/org/myapp/mercury/app/rest/controller/SupplierControllerTest.java
@@ -2,10 +2,13 @@ package org.myapp.mercury.app.rest.controller;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.List;
-
+import static org.mockito.BDDMockito.given;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,7 +18,10 @@ import org.mockito.MockitoAnnotations;
 import org.myapp.mercury.app.model.entity.logistic.Supplier;
 import org.myapp.mercury.app.rest.config.MercuryConfig;
 import org.myapp.mercury.app.rest.config.MercuryInitializer;
+import org.myapp.mercury.app.rest.dto.SupplierDTO;
 import org.myapp.mercury.app.service.LogisticService;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
@@ -28,6 +34,8 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 @ContextConfiguration(classes = { MercuryConfig.class,
 		MercuryInitializer.class }, loader = AnnotationConfigContextLoader.class)
 public class SupplierControllerTest {
+
+	List<Supplier> suppliers;
 
 	private MockMvc mockMvc;
 
@@ -43,10 +51,16 @@ public class SupplierControllerTest {
 		mockMvc = MockMvcBuilders.standaloneSetup(supplierController).build();
 	}
 
+	/**
+	 * If database is empty return status NotFound If database is not empty must
+	 * return status is OK
+	 * 
+	 * @throws Exception
+	 */
 	@Test
-	public void testGetAllSupplierSuccess() throws Exception {
+	public void testGetAllSupplier() throws Exception {
 		List<Supplier> suppliers = logisticService.findSuppliers();
 		when(logisticService.findSuppliers()).thenReturn(suppliers);
-		mockMvc.perform(get("/suppliers")).andExpect(status().isOk());
+		mockMvc.perform(get("/suppliers").accept(MediaType.APPLICATION_JSON)).andExpect(status().isNotFound());
 	}
 }
